a4c1c06999c813838c8e6b5ae473d10b23b954f5
LyLme <admin@lylme.com>
2024-01-17 23:01:31 +0800

    优化60s api

--- api/60s/index.php
+++ api/60s/index.php
@@ -8,11 +8,12 @@
  * @Description: 60秒读懂世界api接口
  */
 header("Content-type:application/json");
-require '../../common/Today.php';
+require '../../common/common.php';
 use lylme\today\Today;
 
-$cachefile = SYSTEM_ROOT.'data/60s_view/cache.json';
+$cachefile = SYSTEM_ROOT . 'data/60s_view/cache.json';
 $cache = json_decode(file_get_contents($cachefile), true);
-$Today = new Today;
-$data = $Today->returnJson($Today->get60sView(),$cache['time']);
+$cache_time = isset($cache['time']) ? $cache['time'] : 0;
+$Today = new Today();
+$data = $Today->returnJson($Today->get60sView(), $cache_time);
 exit($data);
